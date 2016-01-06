74348c480bc37510916ab8e250a0769684766bb4
nevethap <nevethap@INnevethap.local>
2016-01-06 15:19:21 +0530

    wrote a dummy test and added dependencies for mysql and angularjs

---
+++ tests/form-controller-test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect,
+	  app = require('../app/new-account-form/form-controller');
+
+    describe('oneEqualsOne', function () {
+      it('should return true when one equals one', function () {
+        expect(1).to.equal(1);
+      });
+    });
