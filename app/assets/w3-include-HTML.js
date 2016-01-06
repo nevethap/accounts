a38983eeb8772dc8e878aa998066afb1f60cf994
nevethap <nevethap@INnevethap.local>
2016-01-06 14:36:17 +0530

    rectified errors thrown by jshint

--- app/assets/w3-include-HTML.js
+++ app/assets/w3-include-HTML.js
@@ -4,20 +4,23 @@ myHTMLInclude();
 
 function myHTMLInclude() {
   var z, i, a, file, xhttp;
+
+  function setInnerHTML() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      a.removeAttribute("w3-include-html");
+      a.innerHTML = xhttp.responseText;
+      z[i].parentNode.replaceChild(a, z[i]);
+      myHTMLInclude();
+    }
+  }
+  
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     if (z[i].getAttribute("w3-include-html")) {
       a = z[i].cloneNode(false);
       file = z[i].getAttribute("w3-include-html");
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-          a.removeAttribute("w3-include-html");
-          a.innerHTML = xhttp.responseText;
-          z[i].parentNode.replaceChild(a, z[i]);
-          myHTMLInclude();
-        }
-      }
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = setInnerHTML;
       xhttp.open("GET", file, true);
       xhttp.send();
       return;
