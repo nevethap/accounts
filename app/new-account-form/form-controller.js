127863c86b61e7ce6267c72cb065e9e3b46f1dbf
nevethap <nevethap@INnevethap.local>
2016-01-11 10:05:43 +0530

    added a form controller and renamed form-app to form-controller

---
+++ app/new-account-form/form-controller.js
@@ -0,0 +1,7 @@
+(function() {
+var app = angular.module('form', [ ]);
+
+app.controller("form-controller", function() {
+  this.accounts = new_accounts;
+});
+})();
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('app');
 var browserWindow = require('browser-window');
 
+
 app.on('ready', function() {
     var mainWindow = new browserWindow({
       width : 800,
